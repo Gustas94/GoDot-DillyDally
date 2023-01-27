fcc46ac1523f621407bbf7726b25470b90f3c0e8
mkb2001 <m.k.b.2001@hotmail.com>
2023-01-27 01:15:53 +0100

    trolololololo

--- Scenes/MainScenes/SceneHandler.gd
+++ Scenes/MainScenes/SceneHandler.gd
@@ -1,19 +1,25 @@
 extends Node
 
 func _ready():
-	get_node("MainMenu/MarginContainer/VBox/New Game").connect("pressed", self, "on_new_game_pressed")
+	get_node("MainMenu/MarginContainer/VBox/Story").connect("pressed", self, "on_Story_pressed")
 	get_node("MainMenu/MarginContainer/VBox/Quit").connect("pressed", self, "on_Quit_pressed")
 	get_node("MainMenu/MarginContainer/VBox/About").connect("pressed", self, "on_About_pressed")
-	get_node("MainMenu/MarginContainer/VBox/Settings").connect("pressed", self, "on_Settings_pressed")
+	get_node("MainMenu/MarginContainer/VBox/Infinite").connect("pressed", self, "on_Infinite_pressed")
 	
-func on_new_game_pressed():
+func on_Story_pressed():
 	get_node("MainMenu").queue_free()
 	var game_scene = load("res://Scenes/MainScenes/GameScene.tscn").instance()
 	add_child(game_scene)
 
 func on_About_pressed():
-	pass
-	
+	#get_node("MainMenu").queue_free()
+	var About_scene = load("res://Scenes/MainMenu/About.tscn").instance()
+	add_child(About_scene)
+
+func on_Infinite_pressed():
+	get_node("MainMenu").queue_free()
+	var game_scene = load("res://Scenes/MainScenes/GameScene.tscn").instance()
+	add_child(game_scene)
 
 
 func on_Quit_pressed():
